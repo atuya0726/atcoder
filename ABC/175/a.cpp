e654044630d084eabf0c50214e1ee0df917af818
atuya <atuya07261008@gmail.com>
2022-11-07 23:40:14 +0900

    175 a,b

--- ABC/175/a.cpp
+++ ABC/175/a.cpp
@@ -3,11 +3,19 @@ using namespace std;
 
 
 int main() {
-  int n;
-  cin >> n;
+  string S;
+  cin >> S;
 
   int ans = 0;
-
+  int cnt = 0;
+  for(int i = 0; i < S.size(); i++){
+    if(S[i] == 'R'){
+      cnt++;
+      ans = max(cnt,0);
+    }else{
+      cnt = 0;
+    } 
+  }
   cout << ans << endl;
   return 0;
 }
--- ABC/175/b.cpp
+++ ABC/175/b.cpp
@@ -5,8 +5,24 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-
   int ans = 0;
+  vector <int> A(n);
+  
+  for(int i=0;i<n;i++){
+    cin >> A[i];
+  }
+  sort(A.begin(), A.end());
+  for(int i=0;i<n;++i){
+    for(int j=0;j<i;++j){
+      for(int k=0;k<j;++k){
+        if (A[k] != A[j] and A[i] != A[j] and A[k] + A[j] > A[i]) {
+          ans++;
+        }
+      }
+    }
+  }
+
+  
 
   cout << ans << endl;
   return 0;
